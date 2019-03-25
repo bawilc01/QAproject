0095ddb40242c893a64eaee00fc417cd01c0fb67
Brittney <3944682+bawilc01@users.noreply.github.com>
2019-03-24 21:23:03 -0400

    Fixed javascript and css for blog previewer
    Final changes for code louisville project. updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # QA Project
-Blog for QA
+The idea behind the project is having a blog area to talk about being a QA; the fun parts and the challenges.
+
+The home page is where the blog posts from the QA Blog page would go after clicking Submit. Visitors to the site would only see this page if there were a backend with 'user roles' that identified who was a visitor and who was a site owner.
+
+The QA Blog post would be for me only, if there were a back end with user roles as stated before. This is where the javascript is. There are hidden buttons and divs that won't be visible until Preview is clicked. You can enter a title and a text and see a preview of what you've typed as that text is pulled from the title and topic fields. The css incorporates word wrap so the text doesn't spill out of the div.
+
+
+QA Blog Page: Steps to see a Blog Preview:
+
+1) Enter a title for your blog post.
+
+2) Enter some text for your post.
+
+3) Click the Preview button to see a preview of your post before "publishing." There should be a message saying to preview your work and also, the Submit button is visible.
+
+4.1) You can change your text and click Preview again, or...
+
+4.2) Click Submit to "submit" your blog for "publishing."
+
+5) Click "Refresh" to refresh the page and enter another blog post preview.
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,15 +1,36 @@
+/*Blog Previewer*/
+
 const successButton = document.getElementById('successButton').style.display = 'none';
+const refreshButton = document.getElementById('refreshButton').style.display = 'none';
+
+
+var title = document.getElementById('blogTitleInput');
+var topic = document.getElementById('blogContent');
 
 function showPreview(){
   document.getElementById('previewMessage').innerHTML = "Please preview your changes before saving.";
   document.getElementById('successButton').style.display = 'block';
+
+  document.getElementById('blogTitlePreview2').innerHTML = title.value;
+  document.getElementById('blogTitlePreview2').style.display = 'block';
+  document.getElementById('blogContentPreview2').innerHTML = topic.value;
+  document.getElementById('blogContentPreview2').style.display = 'block';
 }
 
-
 function showSuccess() {
+
+  /*hides submit and preview buttons after clicking submit button*/
   document.getElementById('successMessage').innerHTML = "Your changes have been saved!";
   document.getElementById('previewMessage').style.display = 'none';
   document.getElementById('previewButton').style.display = 'none';
   document.getElementById('successButton').style.display = 'none';
+
+  /*show refresh button to refresh page*/
+  document.getElementById('refreshButton').style.display = 'block';
+
+
+  /*hide last preview after submit*/
+  document.getElementById('blogTitlePreview2').style.display = 'none';
+  document.getElementById('blogContentPreview2').style.display = 'none';
 
 }
