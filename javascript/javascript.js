7171424e9b38c5eddab994cd23a4df7fdbb2447e
Brittney <3944682+bawilc01@users.noreply.github.com>
2019-03-24 23:16:36 -0400

    cleaned up spacing and added clearer comments

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,22 +1,29 @@
-/*Blog Previewer*/
+/****************************
+Blog Previewer Functionality
+*****************************/
 
+// Hidden success and refresh buttons on initial page load
 const successButton = document.getElementById('successButton').style.display = 'none';
 const refreshButton = document.getElementById('refreshButton').style.display = 'none';
 
-
+// Assigning text typed in title and topic fields into variables named the same
 var title = document.getElementById('blogTitleInput');
 var topic = document.getElementById('blogContent');
 
+// On Prevew button click functionality - preview message and success button visible
 function showPreview(){
   document.getElementById('previewMessage').innerHTML = "Please preview your changes before saving.";
   document.getElementById('successButton').style.display = 'block';
 
+  // moving text from title and topic area to the blog preview and making it visible for user to preview
   document.getElementById('blogTitlePreview2').innerHTML = title.value;
   document.getElementById('blogTitlePreview2').style.display = 'block';
   document.getElementById('blogContentPreview2').innerHTML = topic.value;
   document.getElementById('blogContentPreview2').style.display = 'block';
 }
 
+
+// clicking Success shows Refresh button and success message
 function showSuccess() {
 
   /*hides submit and preview buttons after clicking submit button*/
